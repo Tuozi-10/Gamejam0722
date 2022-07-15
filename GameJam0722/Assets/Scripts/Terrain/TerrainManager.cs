1e257a1b9cb0deae39f4f446ec8dafa1fa4dbd54
Pierkot <yoann59491@yahoo.fr>
2022-07-15 21:51:08 +0200

    Create terrain script

---
+++ GameJam0722/Assets/Scripts/Terrain/TerrainManager.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour {
+    [SerializeField] private List<GameObject> cubeList = new List<GameObject>();
+    
+}

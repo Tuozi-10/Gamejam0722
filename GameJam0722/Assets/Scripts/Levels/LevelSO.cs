92aaf3778bef60199f7c8907743e63d58b1c4cf9
Pierkot <yoann59491@yahoo.fr>
2022-07-16 01:25:03 +0200

    Level Creation (load, save, generate new one)

--- GameJam0722/Assets/Scripts/Levels/LevelSO.cs
+++ GameJam0722/Assets/Scripts/Levels/LevelSO.cs
@@ -1,18 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Custom SO/New Level")]
+[CreateAssetMenu(menuName = "Custom SO/New Level")]
 public class LevelSO : ScriptableObject {
     [SerializeField] private Vector2 terrainSize = new Vector2(0, 0);
+    public Vector2 TerrainSize => terrainSize;
     [SerializeField] private List<DiceClass> diceClass = new List<DiceClass>();
+    public List<DiceClass> DiceClass => diceClass;
+
 
     /// <summary>
     /// Set all data for the level
     /// </summary>
-    /// <param name="terrainSize"></param>
+    /// <param name="levelSize"></param>
     /// <param name="dicesClass"></param>
-    public void SaveLevelData(Vector2 terrainSize, List<DiceClass> dicesClass) {
-        this.terrainSize = terrainSize;
+    public void SaveLevelData(Vector2 levelSize, List<DiceClass> dicesClass) {
+        this.terrainSize = levelSize;
         this.diceClass = dicesClass;
     }
 }

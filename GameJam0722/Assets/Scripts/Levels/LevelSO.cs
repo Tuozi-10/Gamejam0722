c324f362c706b00c775d7cf4133286822391b3db
Pierkot <yoann59491@yahoo.fr>
2022-07-15 23:52:19 +0200

    Start working on level creation

--- GameJam0722/Assets/Scripts/Levels/LevelSO.cs
+++ GameJam0722/Assets/Scripts/Levels/LevelSO.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Custom SO/New Level")]
 public class LevelSO : ScriptableObject {
     [SerializeField] private Vector2 terrainSize = new Vector2(0, 0);
-    
-    
-    
-    public void CreateNewLevel(Vector2 terrainSize) {
-        terrainSize = this.terrainSize;
+    [SerializeField] private List<DiceClass> diceClass = new List<DiceClass>();
+
+    /// <summary>
+    /// Set all data for the level
+    /// </summary>
+    /// <param name="terrainSize"></param>
+    /// <param name="dicesClass"></param>
+    public void SaveLevelData(Vector2 terrainSize, List<DiceClass> dicesClass) {
+        this.terrainSize = terrainSize;
+        this.diceClass = dicesClass;
     }
 }
--- GameJam0722/Assets/Scripts/Managers/LevelCreationManager.cs
+++ GameJam0722/Assets/Scripts/Managers/LevelCreationManager.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
 
-public class LevelCreationManager : MonoBehaviour
-{
+public class LevelCreationManager : MonoBehaviour {
+   
     
+    [SerializeField] private LevelSO loadLevelSO = null;
+    public LevelSO LoadLevelSO => loadLevelSO;
+
+    [SerializeField] private Vector2 levelSize = new Vector2(1, 1);
+    [SerializeField] private GameObject dicePrefab = null;
+    
+
+    /// <summary>
+    /// Load a level based on a scriptableObject
+    /// </summary>
     public void LoadLevel() {
+        
+    }
+
+    /// <summary>
+    /// Save the actual state of the level in a new SO
+    /// </summary>
+    public void SaveActualLevel() {
+        
+    }
+
+    /// <summary>
+    /// Generate a new level
+    /// </summary>
+    public void GenerateNewLevel() {
         
     }
 }
